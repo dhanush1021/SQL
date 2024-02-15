c264a1abb45f7ee83e8735bdb0bf71ae21ec1a34
Dhanush <dhanushdybala10@gmail.com>
2024-02-15 17:16:13 +0530

    [Dhanush] Add : Inserting values into employee table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,12 +6,16 @@ use Addressbook;*/
 						name varchar(20) not null,
 						salary decimal(10,2) not null,
 						job varchar(20),
-						phone int unique,
+						phone varchar(20) unique,
 						deptid int not null
-						);*/
+						);
 
 create table department_info(
 								deptid int not null,
 								deptname varchar(20) not null,
 								deptlocation varchar(50)
 							); 
+							*/
+
+insert into employee values(01,'Adam',25000,'Jr. Developer','9876543209',10);
+insert into employee values(02,'Sunil',20000,'Jr. Developer','8765432109',20);
